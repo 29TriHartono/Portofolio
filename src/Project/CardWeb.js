190332db8bb5011f65e53a59645d7b1aa3f8aa89
29TriHartono <29TriHartono@gmail.com>
2022-06-21 00:42:25 +0700

    Change Link

--- src/Project/CardWeb.js
+++ src/Project/CardWeb.js
@@ -9,17 +9,23 @@ import Home from '../component/Home';
 function CardWeb() {
   return (
     <div className="grid md:grid-cols-2 gap-4 md:gap-10">
-      <Card imageUrl={naufalSatu} title="Design by Naufal@array.id" ket="Slicing Design array id (Tailwindcss & Aos liblary)" toUrl="https://trihartonoo.github.io/Arrayid-MSTSKP/" toGithub="https://github.com/Trihartonoo/Arrayid-MSTSKP" />
+      <Card
+        imageUrl={naufalSatu}
+        title="Design by Naufal@array.id"
+        ket="Slicing Design array id (Tailwindcss & Aos liblary)"
+        toUrl="https://29TriHartono.github.io/Arrayid-Mstskp/"
+        toGithub="https://github.com/29TriHartono/Arrayid-Mstskp"
+      />
       <Card
         imageUrl={naufalDua}
         title="Design by Naufal@array.id"
         ket="Slicing Design array id (Html,Tailwindcss,Js & Aos liblary)"
-        toUrl="https://trihartonoo.github.io/Arrayid-Wumbo/"
-        toGithub="https://github.com/Trihartonoo/Arrayid-Wumbo"
+        toUrl="https://29TriHartono.github.io/Arrayid-Wumbo/"
+        toGithub="https://github.com/29TriHartono/Arrayid-Wumbo"
       />
 
-      <Card imageUrl={RumahImpian} title="Design by Me" toUrl="https://trihartonoo.github.io/MyPortofolio" toGithub="https://trihartonoo.github.io/MyPortofolio" />
-      <Card imageUrl={JustTravel} title="Design by Me" toUrl="https://trihartonoo.github.io/MyPortofolio" toGithub="https://trihartonoo.github.io/MyPortofolio" />
+      <Card imageUrl={RumahImpian} title="Design by Me" toUrl="https://29TriHartono.github.io/Portofolio" toGithub="https://29TriHartono.github.io/Portofolio" />
+      <Card imageUrl={JustTravel} title="Design by Me" toUrl="https://29TriHartonoo.github.io/Portofolio" toGithub="https://29TriHartono.github.io/Portofolio" />
     </div>
   );
 }
