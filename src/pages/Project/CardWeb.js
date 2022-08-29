94af370d6773fd6585e053e6ec08f4c599ae7717
29trihartono <29trihartono@gmail.com>
2022-08-29 15:37:26 +0700

    link

--- src/pages/Project/CardWeb.js
+++ src/pages/Project/CardWeb.js
@@ -8,8 +8,8 @@ import Nfttheme from '../../Img/Nft-img.png';
 function CardWeb() {
   return (
     <div className="grid md:grid-cols-2 gap-4 md:gap-10">
-      <Card imageUrl={RumahImpian} title="Design by Me" ket="Slicing Design (React Js, Tailwind Css)" toUrl="https://29trihartono.github.io/Rumahimpian" toGithub="https://29TriHartono.github.io/Portofolio" />
-      <Card imageUrl={Nfttheme} title="" ket="Slicing Design (React JS, Bootstrap)" toUrl="https://29trihartono.github.io/Nft-theme/#/home" toGithub="https://github.com/29TriHartono/Nft-theme" />
+      <Card imageUrl={RumahImpian} title="Design by Me" ket="Slicing Design (React Js, Tailwind Css)" toUrl="https://rumah-impian.vercel.app/beranda" toGithub="https://29TriHartono.github.io/Portofolio" />
+      <Card imageUrl={Nfttheme} title="" ket="Slicing Design (React JS, Bootstrap)" toUrl="https://nft-theme.vercel.app/home" toGithub="https://github.com/29TriHartono/Nft-theme" />
       <Card
         imageUrl={naufalSatu}
         title="Design by Naufal@array.id"
@@ -17,15 +17,9 @@ function CardWeb() {
         toUrl="https://29TriHartono.github.io/Arrayid-Mstskp/"
         toGithub="https://github.com/29TriHartono/Arrayid-Mstskp"
       />
-      <Card
-        imageUrl={naufalDua}
-        title="Design by Naufal@array.id"
-        ket="Slicing Design array id (Html,Tailwindcss,Js & Aos liblary)"
-        toUrl="https://29TriHartono.github.io/Arrayid-Wumbo/"
-        toGithub="https://github.com/29TriHartono/Arrayid-Wumbo"
-      />
+      <Card imageUrl={naufalDua} title="Design by Naufal@array.id" ket="Slicing Design array id (Html,Tailwindcss,Js & Aos liblary)" toUrl="https://arrayid-wumbo.vercel.app/" toGithub="https://github.com/29TriHartono/Arrayid-Wumbo" />
 
-      <Card imageUrl={JustTravel} title="Design by Me" toUrl="https://29TriHartonoo.github.io/Portofolio" toGithub="https://29TriHartono.github.io/Portofolio" />
+      <Card imageUrl={JustTravel} title="Design by Me" toUrl="https://arrayid-mstskp.vercel.app/" toGithub="https://29TriHartono.github.io/Portofolio" />
     </div>
   );
 }
